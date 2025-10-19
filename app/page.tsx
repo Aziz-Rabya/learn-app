7a541df7db5bd6de4bb1ee92341b8ebb76b2bf92
Aziz-Rabya <azizrabya81@gmail.com>
2025-10-19 16:31:34 +0100

    impement home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,49 @@
-
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import {recentSessions} from "@/constants";
 
 const Page = () => {
   return (
-    <div>
+    <main>
+      <h1 className="text-2xl underline">Dashboard</h1>
 
-    </div>
-  )
-}
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="Science"
+          duration={45}
+          color="#E5D0FF"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="Maths"
+          duration={30}
+          color="#FFDA6E"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature "
+          subject="Language"
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+      <section className="home-section">
+        <CompanionsList 
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max--lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
+  );
+};
 
-export default Page
+export default Page;
